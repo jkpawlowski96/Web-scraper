89e6e266d3b69561f83520678418d03459f8e6ed
Jakub <jkpawlowski96@gmail.com>
2019-08-25 19:26:44 +0200

    images path

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, Response
 from bs4 import BeautifulSoup
-import urllib.request as urllib2
+import urllib.request as request
 import image_scraper
 from data import *
 
@@ -10,27 +10,54 @@ app= Flask(__name__)
 
 @app.route("/")
 def home():
-    return "hello world"
+	return "hello world"
 
 """-----------------------------------------------
 Command service to download resources form website
 """
+@app.route('/order/<path:adress>')
+def order(adress):
+	web_text = get_text(adress)
+	web_images = get_images(adress)
+	if data_add(adress=adress,text=web_text,images=web_images) is True: #and images in the future 
+		return 'succes'
+	else:
+		return 'FAILED!!!'
+	
+	#return web_text
 
+"""-----------------------------------------------
+Command service to download resources form website
+"""
+@app.route('/resources/display/')
+def display(): 
+	return data_display()
 
-@app.route('/t/<path:adress>')
+@app.route('/resources/display/images')
+def display_images(): 
+	return data_images_display()
+
+@app.route('/resources/display/text')
+def display_text(): 
+	return data_text_display()
+
+"""-----------------------------------------------
+Download resources form website
+"""
+@app.route('/file/t/<path:adress>')
 def download_text(adress):
-    page = urllib2.urlopen(adress)
-    web_text = get_text(page)
-    
-    return string_to_file(web_text)
-    #return web_text
+	page = request.urlopen(adress)
+	web_text = get_text(page)
+	
+	return string_to_file(web_text)
+	#return web_text
 
-@app.route('/i/<path:adress>')
+@app.route('/file/i/<path:adress>')
 def download_images(adress):
-    data = image_scraper.scrape_images(adress)
+	data = image_scraper.scrape_images(adress)
 
-    return string_to_file(str(data))
-    #return web_text
+	return string_to_file(str(data))
+	#return web_text
 
 """------------------------------------------------
 Downlaod all resources as csv file
@@ -41,27 +68,43 @@ Downlaod all resources as csv file
 
 
 def string_to_file(data):
-        return Response(
-        data,
-        mimetype="text/txt",
-        headers={"Content-disposition":
-                 "attachment; filename=file.txt"})
+	return Response(
+	data,
+	mimetype="text/txt",
+	headers={"Content-disposition":
+			"attachment; filename=file.txt"})
 
 def images_to_file(data):
-        return Response(data,
-            mimetype='application/zip',
-            headers={'Content-Disposition':'attachment;filename=images.zip'})
+	return Response(data,
+		mimetype='application/zip',
+		headers={'Content-Disposition':'attachment;filename=images.zip'})
 
-def get_text(html):
-    soup = BeautifulSoup(html, 'html.parser')
-    #soup.find_all("div")
-    web_text = ''
-    for div in soup.find_all("div"):
-        web_text += div.text+' '
-    #page = bs4.BeautifulSoup(html, 'html')
-    #web_text = page.get_text(separator='\n')
-    web_text = web_text.replace('\\n','')
-    web_text = web_text.replace('\n','')
-    web_text = web_text.replace('  ','')
+def get_text(adress):
+	page = request.urlopen(adress)
+	soup = BeautifulSoup(page, 'html.parser')
+	#soup.find_all("div")
+	web_text = ''
+	for div in soup.find_all("div"):
+		web_text += div.text+' '
+	#page = bs4.BeautifulSoup(html, 'html')
+	#web_text = page.get_text(separator='\n')
+	web_text = web_text.replace('\\n','')
+	web_text = web_text.replace('\n','')
+	web_text = web_text.replace('  ','')
 
-    return web_text
+	return web_text
+
+
+def get_images(adress):
+	page = request.urlopen(adress)
+	soup = BeautifulSoup(page, 'html.parser')
+	tags=soup.findAll('img')
+	print(tags)
+	images = []
+	for img in tags:
+		try:
+			images.append(img['src'])
+		except:
+			pass
+		
+	return images
--- app/data.py
+++ app/data.py
@@ -1,11 +1,24 @@
 import pandas as pd
 import numpy as np
 
-data_global = pd.DataFrame(columns=['Adress','Text','Images'])
+data = pd.DataFrame(columns=['Adress','Text','Images'])
 
-def data_add(adress=np.nan, test=np.nan, images=np.nan, df=data_global):
+def data_add(adress=np.nan, text=np.nan, images=np.nan):
+    global data
     try:
-        df = df.append({'Adress': adress,'Text':text, 'Images':images}, ignore_index=True)
+        data = data.append({'Adress': adress,'Text':text, 'Images':images}, ignore_index=True)
         return True
     except:
         return False
+def data_display():
+    global data
+    return data.to_html()
+    
+def data_images_display():
+    global data
+    
+    return data[['Adress','Images']].to_html()
+
+def data_text_display():
+    global data
+    return data[['Adress','Text']].to_html()
