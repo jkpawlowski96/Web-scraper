822d9e548557b1b29c59a53b24524ac68e57576f
jk <jkpawlowski96@gmail.com>
2019-08-29 02:15:31 +0200

    rev

--- app/task.py
+++ app/task.py
@@ -1,58 +1,74 @@
 from text import get_text, string_to_file
-from images import get_images_bytes, get_images_links
-from database import data_add_resource, adress_done, adress_working
+from images import get_images_bytes, get_images_links 
+from database import data_add_resource, address_done, address_working
 import threading
 import time
-from random import randint
-
-"""Queue of adress to scrap and add resources to database"""
 
 
-def init_Scrapper():
+def init_scrapper():
+	"""
+	Initialize background thread to scrap websites
+	:return: thread
+	"""
 	mythread = Scrapper(name = "Thread-{}".format(1))  # ...Instantiate a thread and pass a unique ID to it
 	mythread.start()                                   # ...Start the thread, invoke the run method
 	return mythread
 
 
-def scrap_website(adress):	
-	web_text = get_text(adress)
-	web_images_l = get_images_links(adress)
-	web_images_b = get_images_bytes(web_images_l)
-	row = {}
-	row['Adress']=adress
-	row['Text']=web_text
-	row['Images']=web_images_b
-	row['Images_links']=web_images_l
-	if data_add_resource(row) is True: #and images in the future 
+def scrap_website(address):
+	"""
+	Scrap text and images from website
+	:param address: website address example: https://www.youtube.com/
+	:return: true value if done
+	"""
+	web_text = get_text(address)  # scrap text
+	web_images_l = get_images_links(address)  # scrap images
+	web_images_b = get_images_bytes(web_images_l)  # images into bytes
+	row = {'Address': address, 'Text': web_text, 'Images': web_images_b, 'Images_links': web_images_l}
+	#  add resource to database
+	if data_add_resource(row) is True:
 		return True
 	else:
 		return False
 
-class Scrapper(threading.Thread):
 
-	job_queue=[]
+class Scrapper(threading.Thread):
+	"""
+	Scrapping thread class
+	"""
+	# Queue of address to scrap and add resources to database
+	job_queue = []
 
 	def run(self):
-		while(True):
+		"""
+		Loop of the thread
+		:return:
+		"""
+		while():
 			try:
-				adress = self.job_queue.pop(0)  # Take adress to do from the stack
+				address = self.job_queue.pop(0)  # Take address to do from the stack
 			except:
-				time.sleep(2)
+				# no tasks
+				time.sleep(2)  # wait a moment
 				continue
 			try:
-				adress_working(adress,value=True)
-				results = scrap_website(adress)
+				address_working(address, value=True)  # add as working task
+				results = scrap_website(address)  # scrap website
 				if results is True:
-					adress_working(adress,value=False)  # Job is not working anymore. Adress is already in database
+					address_working(address, value=False)  # Job is not working anymore. address is already in database
 					continue
-					# "Thread-x finished!"
 				else:
 					continue  # Something was wrong
 			except:
 				pass
-	
-	def process_order(self, adress):
-		self.job_queue.append(adress)
+
+	def process_order(self, address):
+		"""
+		Take and order to scrap website
+		:param address: website address example: https://www.youtube.com/
+		:return:
+		"""
+		self.job_queue.append(address)
 
 
 
